71651c0be62c34e8c72998ac3b45dbff3c42b3a3
nachikooo <45351188+nachikooo@users.noreply.github.com>
2019-03-04 01:00:36 +0800

    Add files via upload

--- student/models.py
+++ student/models.py
@@ -1,35 +1,49 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Student(models.Model):
-	YEAR_IN_SCHOOL_CHOICES = (
+    YEAR_IN_SCHOOL_CHOICES = (
         ('FR', 'Freshman'),
         ('SO', 'Sophomore'),
         ('JR', 'Junior'),
         ('SR', 'Senior'),
         ('GR', 'Graduate'),
     )
-	COURSE_CHOICES = (
-		('Bio', 'Biology'),
-		('CS', 'Computer Science'),
-		('Math', 'Mathematics'),
-		('PS', 'Political Science'),
-		('Psych', 'Psychology'),
-		('Comm', 'Communications'),
-		('FA', 'Fine Arts'),
-		('Mgt', 'Management'),
-	)
-	s_name = models.CharField(max_length=100)
-	s_number = models.CharField(max_length=20, primary_key=True)
-	contact_no = models.CharField(max_length=11)
-	email_add = models.CharField(max_length=100)
-	s_course = models.CharField(max_length=100, choices=COURSE_CHOICES)
-	s_year_lvl = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
-	
+    COURSE_CHOICES = (
+        ('Bio', 'Biology'),
+        ('CS', 'Computer Science'),
+        ('Math', 'Mathematics'),
+        ('PS', 'Political Science'),
+        ('Psych', 'Psychology'),
+        ('Comm', 'Communications'),
+        ('FA', 'Fine Arts'),
+        ('Mgt', 'Management'),
+    )
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    s_number = models.CharField(max_length=20, primary_key=True)
+    contact_no = models.CharField(max_length=11)
+    email_add = models.CharField(max_length=100)
+    s_course = models.CharField(max_length=100, choices=COURSE_CHOICES)
+    s_year_lvl = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
+
+
+    def __str__(self):
+        if self.first_name and not self.last_name:
+            return self.first_name
+        elif self.first_name and self.last_name:
+            return self.first_name + ' ' + self.last_name
+        else:
+            return 'Student'
+
+
 class Class(models.Model):
-	c_title = models.CharField(max_length=50)
-	c_code = models.CharField(max_length=10,primary_key=True)
-	
-	
-	
+    c_title = models.CharField(max_length=50)
+    c_code = models.CharField(max_length=10, primary_key=True)
+
+    def __str__(self):
+        return self.c_title
+
+
