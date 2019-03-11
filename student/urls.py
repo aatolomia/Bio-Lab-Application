ab50b1d5603182e46beb2f2de0b96736587c7394
aatolomia <47099747+aatolomia@users.noreply.github.com>
2019-03-11 14:42:11 +0800

    comment out path to about

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='student-home'),
-    path('about/', views.about, name='main-about'),
+    #path('about/', views.about, name='main-about'),
 ]
