25e3e2f5578390f26c962672f974a45425a514f4
nachikooo <45351188+nachikooo@users.noreply.github.com>
2019-03-16 05:21:46 +0800

    Update urls.py

--- biolab/biolab/urls.py
+++ biolab/biolab/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('student.urls')),
-    path('register/', user_views.register, name='register'),
+    path('home/', include('student.urls')),
+    path('', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='lilo/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='lilo/logout.html'), name='logout'),
