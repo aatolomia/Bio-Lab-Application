09e2ae2f0709cba51bf05cb50f9b3e82cac384d6
nachikooo <45351188+nachikooo@users.noreply.github.com>
2019-03-17 03:15:21 +0800

    Update forms.py

--- biolab/student/forms.py
+++ biolab/student/forms.py
@@ -1,15 +1,31 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
-	email = forms.EmailField()
-	first_name = forms.CharField()
-	last_name = forms.CharField()
-	student_number = forms.CharField(max_length = 20)
+	username = forms.CharField(widget=forms.TextInput(attrs={'style':'max-width: 20em'}))
+	email = forms.EmailField(widget=forms.TextInput(attrs={'style':'max-width: 20em'}))
+	first_name = forms.CharField(widget=forms.TextInput(attrs={'style':'max-width: 20em'}))
+	last_name = forms.CharField(widget=forms.TextInput(attrs={'style':'max-width: 20em'}))
+	student_number = forms.CharField(max_length = 20, widget=forms.TextInput(attrs={'style':'max-width: 20em'}))
 	
 
 	class Meta:
 		model = User 
 		fields = ['username', 'email', 'first_name', 'last_name', 'student_number', 'password1', 'password2']
+		
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(widget=forms.TextInput(attrs={'style':'max-width: 20em'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'style':'max-width: 20em'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
