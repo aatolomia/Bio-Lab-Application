dc8f62e00f9937896508f425474602555192ae64
nachikooo <45351188+nachikooo@users.noreply.github.com>
2019-03-17 03:18:02 +0800

    Update urls.py

--- biolab/biolab/urls.py
+++ biolab/biolab/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('home/', include('student.urls')),
     path('', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
+    path('updateProfile/', user_views.updateProfile, name='updateProfile'),
     path('login/', auth_views.LoginView.as_view(template_name='lilo/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='lilo/logout.html'), name='logout'),
 ]
