e902ef88918915936d68896d0600dcc7a331d7b4
larskii12 <45351038+larskii12@users.noreply.github.com>
2019-03-18 23:08:15 +0800

    Add files via upload
    Presented this version of the application

--- student/models.py
+++ student/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
 
@@ -23,12 +26,12 @@ class Student(models.Model):
     )
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
-    s_number = models.CharField(max_length=20, primary_key=True)
-    contact_no = models.CharField(max_length=11)
+    s_number = models.CharField(max_length=20, null=True, verbose_name='Student Number')
+    contact_no = models.CharField(max_length=11,verbose_name='Contact Number')
     email_add = models.CharField(max_length=100)
-    s_course = models.CharField(max_length=100, choices=COURSE_CHOICES)
-    s_year_lvl = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
-
+    s_course = models.CharField(max_length=100, choices=COURSE_CHOICES,verbose_name='Course')
+    s_year_lvl = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES,verbose_name='Year Level')
+    
 
     def __str__(self):
         if self.first_name and not self.last_name:
@@ -47,3 +50,22 @@ class Class(models.Model):
         return self.c_title
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default = 'default.jpg', upload_to = 'profile_pics')
+    student_number = models.CharField(max_length=30, null = True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'  
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
