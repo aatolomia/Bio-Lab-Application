75252378254fd5eae573828dc0bc290b332b43d8
nachikooo <45351188+nachikooo@users.noreply.github.com>
2019-03-23 04:08:38 +0800

    Update models.py
    Added Model for Updating Profile

--- biolab/student/models.py
+++ biolab/student/models.py
@@ -160,7 +160,14 @@ class Class(models.Model):
 
     def __str__(self):
         return self.c_title
+    
+class updateProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default = 'default.jpg', upload_to = 'profile_pics')
+    student_number = models.CharField(max_length=30, null = True)
 
+    def __str__(self):
+        return f'{self.user.username} Profile'  
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
