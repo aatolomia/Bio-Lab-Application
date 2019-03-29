bd8bdb676a5fa9776cd28f2e37a3acd75bd82bf9
Elyssa1998 <47099726+Elyssa1998@users.noreply.github.com>
2019-03-29 18:20:13 +0800

    added equipment in installed apps

--- biolab/biolab/settings.py
+++ biolab/biolab/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'equipment.apps.EquipmentConfig',
     'student.apps.StudentConfig',
     'crispy_forms',
     'django.contrib.admin',
