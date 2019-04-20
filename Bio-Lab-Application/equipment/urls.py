8b85bc676659d0359854d0e5c8ab31021150c711
Elyssa1998 <47099726+Elyssa1998@users.noreply.github.com>
2019-04-21 01:33:25 +0800

    Update urls.py

--- Bio-Lab-Application/equipment/urls.py
+++ Bio-Lab-Application/equipment/urls.py
@@ -9,13 +9,8 @@ urlpatterns = [
 	path('', views.equipment, name='bio-app-equipment'),
 	
 	#/equipment/<equipmenttype_id>/ but i want it to be equipment/glasswares
-	url(r'^(?P<EquipmentType_id>[0-9]+)/$', views.detail, name='bio-app-detail'),
+	path('<int:EquipmentType_id>/', views.detail, name='bio-app-detail'),
 
 	#/equipment/<equipmenttype_id>/<equipment_id>/
-	url(r'^(?P<EquipmentType_id>[0-9]+)/(?P<Equipment_id>[0-9]+)/$', views.item, name='bio-app-item'),
-
-	#/equipment/<equipmenttype_id>/favorite/
-	#url(r'^(?P<EquipmentType_id>[0-9]+)/favorite/$', views.favorite, name='bio-app-favorite'),
+	path('<int:EquipmentType_id>/<int:Equipment_id>/', views.item, name='bio-app-item'),
 ]
-
-#path('<int: EquipmentType_id>/', views.detail, name='bio-app-detail'),
