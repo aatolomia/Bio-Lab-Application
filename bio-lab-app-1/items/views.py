5ebdc4b8022a524e3f6f7a4ed0a9af44d6371fa0
nachikooo <45351188+nachikooo@users.noreply.github.com>
2019-05-17 14:20:05 +0800

    Update views.py

--- bio-lab-app-1/items/views.py
+++ bio-lab-app-1/items/views.py
@@ -40,22 +40,57 @@ def items(request):
     categories = Category.objects.all()
     return render(request, "items/items.html", {"items": items, "categories": categories})
 
-def request(request):
-    pend = Pending.objects.all()
-    if request.method == "POST":
-        
-        items_id = request.POST.get('select')
-        pend = Pending.objects.get(pk=items_id)
-        borrow = Borrow()
-        borrow.borrower = pend.user_borrow
-        borrow.name_item = pend.name_of_item
-        borrow.picture = pend.picture
-        borrow.date = datetime.now()
-        borrow.save()
-        pend.delete()
-        return redirect('/request')
 
-    return render(request, "items/request.html", {"pend": pend})
+@login_required
+def request(request):
+	pend = Pending.objects.all()
+	#items = products.filter(name=name)
+	if request.method == "POST":
+
+        
+		items_id = request.POST.get('select')
+
+		num = request.POST.get('num')
+		if num and items_id:
+           
+			pend = get_object_or_404(Pending, pk=items_id)
+			borrow = Borrow()
+			borrow.borrower = pend.user_borrow
+			borrow.name_item = pend.name_of_item   
+			borrow.picture = pend.picture
+			borrow.date = datetime.now()
+            
+			try:
+				num = int(request.POST.get('num'))
+				borrow.num_of_items = request.POST.get('num')
+				pend.num_of_items = pend.num_of_items-num
+
+				
+				items = Item.objects.get(name=pend.name_of_item)
+				items.available = items.available + pend.num_of_items
+				items.save()
+
+				if pend.num_of_items < 0:
+					messages.error(request, 'Invalid')
+					return redirect('/request')
+				if num < 0:
+					messages.error(request, 'Invalid')
+					redirect('/request')
+				else:
+					#it.save()
+					borrow.save()
+					pend.delete()
+					if pend.num_of_items == 0:
+						pend.delete()
+					return redirect('/request')
+			except ValueError:
+				messages.error(request, 'Invalid')
+				return redirect('/request')
+		else:
+			return redirect('/request')
+
+	return render(request, "items/request.html", {"pend": pend})
+
 
 def delete_category(request, id):
     category = Category.objects.filter(id=id)
@@ -83,30 +118,42 @@ def approve(request):
     return render(request, "items/request.html")
 
 
+@login_required
 def borrow(request):
     items = Item.objects.all()
     if request.method == "POST":
         
         status = "Borrowed"
         items_id = request.POST.get('selector')
-        
-        
-        pend = Item.objects.get(pk=items_id)
-        it = Item.objects.get(pk=items_id)
-        pending = Pending()
-        pending.user_borrow = request.user
-        pending.name_of_item = pend.name
-        #pending.num_borrowed = pend.available-1
-        pending.picture = pend.picture
-        it.available = pend.available-1
-        #instance = cart.save(commit=False)
-        #instance.user_b = request.user
-        it.save()
-        pending.save()
-        return redirect("/borrow")
+        print(items_id)
+      
+        num = request.POST.get('num')
+        if num and items_id:
+            num = int(request.POST.get('num'))
+            pend = get_object_or_404(Item, pk=items_id)
+            it = get_object_or_404(Item, pk=items_id)
+            pending = Pending()
+            pending.user_borrow = request.user
+            pending.name_of_item = pend.name
+            pending.num_of_items = request.POST.get('num')
+            pending.picture = pend.picture
+            it.available = pend.available-num
+            print(it.available)
+            if it.available < 0:
+                messages.error(request,'Invalid')
+                return redirect("/borrow")
+
+            else:
+                it.save()
+                pending.save()
+                return redirect("/borrow")
+        else:
+            return redirect("/borrow")
+       
   #  students = Student.objects.all()
-    
+     
     return render(request, "items/borrow.html", {'items':items})
+
 
 
 def approvereturn(request):
